5547643084b1559708716f06a0f6e0ebd16997aa
vmukund <v_mukund@yahoo.com>
2024-09-14 19:12:17 +0530

    Create OpenTelemetryManager.java -Initial commit
     Adds a BatchSpanProcessor initialized with OtlpGrpcSpanExporter to the  TracerSdkProvider.

---
+++ SprintSpan_Annotation/demo/src/main/java/com/example/emittrace/OpenTelemetryManager.java
@@ -0,0 +1,58 @@
+package com.example.emittrace;
+
+import java.util.concurrent.TimeUnit;
+
+import io.opentelemetry.api.OpenTelemetry;
+import io.opentelemetry.api.common.AttributeKey;
+import io.opentelemetry.api.common.Attributes;
+import io.opentelemetry.exporter.otlp.trace.OtlpGrpcSpanExporter;
+import io.opentelemetry.sdk.OpenTelemetrySdk;
+import io.opentelemetry.sdk.resources.Resource;
+import io.opentelemetry.sdk.trace.SdkTracerProvider;
+import io.opentelemetry.sdk.trace.export.BatchSpanProcessor;
+
+/**
+ * All SDK management takes place here, away from the instrumentation code,
+ * which should only access the OpenTelemetry APIs.
+ */
+public final class OpenTelemetryManager {
+
+        /**
+         * Adds a BatchSpanProcessor initialized with OtlpGrpcSpanExporter to the
+         * TracerSdkProvider.
+         *
+         * @return a ready-to-use {@link OpenTelemetry} instance.
+         */
+
+        public static OpenTelemetry initOpenTelemetry() {
+
+                OtlpGrpcSpanExporter spanExporter = OtlpGrpcSpanExporter.builder().setTimeout(2, TimeUnit.SECONDS)
+                                .build();
+                BatchSpanProcessor spanProcessor = BatchSpanProcessor.builder(spanExporter)
+                                .setScheduleDelay(100, TimeUnit.MILLISECONDS).build();
+
+                Resource serviceName = Resource
+                                .create(Attributes.of(AttributeKey.stringKey("service.name"),
+                                                "TraceSpanService"));
+                // SdkTracerProvider tracerProvider =
+                // SdkTracerProvider.builder().addSpanProcessor(spanProcessor)
+                // .setResource(OpenTelemetrySdkAutoConfiguration.getResource().merge(serviceName))
+                // .build();
+
+                SdkTracerProvider tracerProvider = SdkTracerProvider.builder().addSpanProcessor(spanProcessor)
+                                .setResource(serviceName)
+                                .build();
+
+                // SdkTracerProvider tracerProvider =
+                // SdkTracerProvider.builder().addSpanProcessor(spanProcessor)
+                // .setResource(serviceName).build();
+
+                OpenTelemetrySdk openTelemetrySdk = OpenTelemetrySdk.builder().setTracerProvider(tracerProvider)
+                                .buildAndRegisterGlobal();
+
+                Runtime.getRuntime().addShutdownHook(new Thread(tracerProvider::close));
+
+                return openTelemetrySdk;
+        }
+
+}
