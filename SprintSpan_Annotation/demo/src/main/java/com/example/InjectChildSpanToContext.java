0e2320a3991432507cf08833c260ab943eedf1c8
vmukund <v_mukund@yahoo.com>
2024-09-14 19:06:23 +0530

    Create InjectChildSpanToContext.java -Initial Commit

---
+++ SprintSpan_Annotation/demo/src/main/java/com/example/InjectChildSpanToContext.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import static java.lang.annotation.ElementType.*;
+import static java.lang.annotation.RetentionPolicy.*;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+@Retention(RUNTIME)
+@Target(METHOD)
+public @interface InjectChildSpanToContext {
+
+    String spanName() default "";
+
+}
