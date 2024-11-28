20579cf4b12f4b3c78d2f6f95681533825da26e6
vmukund <v_mukund@yahoo.com>
2024-11-28 12:22:24 +0530

    Create SyslogsenderApplication.java initial commit

---
+++ SyslogIntegration/RFC5424/SpringBootApp/syslogsender/src/main/java/com/example/SyslogsenderApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyslogsenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SyslogsenderApplication.class, args);
+	}
+
+}
